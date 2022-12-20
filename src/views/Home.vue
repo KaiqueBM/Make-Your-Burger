66a2d4fb739c79ec3ece95ded996deed3504f241
Kaique Miranda <66568882+KaiqueBM@users.noreply.github.com>
2022-12-20 15:32:11 -0300

    Add project

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-container">
+    <Banner />
+    <BurgerForm id="main-burgerform" />
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue';
+import BurgerForm from '../components/BurgerForm.vue';
+
+export default{
+  name: "Home",
+  components: {
+    Banner,
+    BurgerForm
+  }
+}
+
+
+</script>
+
+<style scoped>
+  #main-burgerform{
+    margin: 50px;
+  }
+</style>
